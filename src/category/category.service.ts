408275b47bdb230895caf55d2996ada00539c91d
pakhdev <t604977944@gmail.com>
2024-10-26 07:34:55 +0200

    Category module - Service created

---
+++ src/category/category.service.ts
@@ -0,0 +1,51 @@
+import { Injectable } from "@nestjs/common";
+import { CreateCategoryDto } from "./dto/create-category.dto";
+import { UpdateCategoryDto } from "./dto/update-category.dto";
+import { User } from "../authorization/entities/user.entity";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
+import { Category } from "./entities/category.entity";
+
+@Injectable()
+export class CategoryService {
+  constructor(
+    @InjectRepository(Category)
+    private readonly categoriesRepository: Repository<Category>,
+  ) {}
+
+  async create(
+    createCategoryDto: CreateCategoryDto,
+    user: User,
+  ): Promise<Category> {
+    const { name } = createCategoryDto;
+    const category = { name, user };
+    return this.categoriesRepository.save(category);
+  }
+
+  async findById(id: number, user: User): Promise<Category> {
+    const category = await this.categoriesRepository.findOne({
+      where: { id, user },
+    });
+    if (!category) throw new Error("Category not found");
+    return category;
+  }
+
+  async findAll(user: User): Promise<Category[]> {
+    return await this.categoriesRepository.find({ where: { user } });
+  }
+
+  async update(
+    id: number,
+    updateCategoryDto: UpdateCategoryDto,
+    user: User,
+  ): Promise<Category> {
+    let category = await this.findById(id, user);
+    const { name } = updateCategoryDto;
+    category = { ...category, name };
+    return await this.categoriesRepository.save(category);
+  }
+
+  async remove(id: number, user: User): Promise<void> {
+    await this.categoriesRepository.delete({ id, user });
+  }
+}
