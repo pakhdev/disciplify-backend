783f789bdee3fee963e1f01fb27f4a3a15920db2
pakhdev <t604977944@gmail.com>
2024-11-04 17:45:55 +0100

    Maintenance Service - Created the main logic for statistic calculations - Added new methods for date retrieval and calculations, along with updates to existing ones

--- src/maintenance/maintenance.service.ts
+++ src/maintenance/maintenance.service.ts
@@ -3,6 +3,7 @@ import { TaskService } from "../task/task.service";
 import { Cron } from "@nestjs/schedule";
 import { AuthorizationService } from "../authorization/authorization.service";
 import { StatisticService } from "../statistic/statistic.service";
+import { User } from "../authorization/entities/user.entity";
 
 @Injectable()
 export class MaintenanceService implements OnModuleInit {
@@ -28,17 +29,82 @@ export class MaintenanceService implements OnModuleInit {
     for (const user of users) {
       console.log(`Maintenance for user ${user.id}`);
       const dates = this.getDateRange(user.statisticDate, today);
-      dates.forEach((date) => {});
+      for (const date of dates) {
+        console.log("DATE", date);
+        await this.performMaintenanceForDate(date, user);
+      }
+      await this.authorizationService.markStatisticDate(new Date(), user);
     }
 
     console.log("Maintenance finished");
   }
 
+  private async performMaintenanceForDate(
+    date: Date,
+    user: User,
+  ): Promise<void> {
+    const tasks = await this.taskService.findByDate(date, user);
+    const dateStatistic = this.statisticService.calculateStatsForDay(tasks);
+
+    const currentDayWeekNumber = this.getWeekNumber(date);
+    const currentDayMonthNumber = this.getMonthNumber(date);
+
+    const nextDay = this.getNextDayDate(date);
+    const nextDayWeekNumber = this.getWeekNumber(nextDay);
+    const nextDayMonthNumber = this.getMonthNumber(nextDay);
+
+    await this.statisticService.saveDayStatistic(date, dateStatistic, user);
+
+    if (currentDayWeekNumber !== nextDayWeekNumber) {
+      const weekRange = this.getWeekRange(date);
+      const weekStatisticValues = await this.statisticService.findByDatesRange(
+        weekRange.startDate,
+        weekRange.endDate,
+        user,
+      );
+      if (weekStatisticValues.length !== 14) return;
+      const weekStatistic =
+        this.statisticService.calculateAverageScore(weekStatisticValues);
+      await this.statisticService.saveWeekStatistic(
+        currentDayWeekNumber,
+        weekStatistic,
+        user,
+      );
+    }
+
+    if (currentDayMonthNumber !== nextDayMonthNumber) {
+      const monthRange = this.getMonthRange(date);
+      const monthStatisticValues = await this.statisticService.findByDatesRange(
+        monthRange.startDate,
+        monthRange.endDate,
+        user,
+      );
+      if (
+        monthStatisticValues.length !==
+        this.getDaysInMonth(
+          this.getYearNumber(date),
+          this.getMonthNumber(date),
+        ) *
+          2
+      )
+        return;
+
+      const monthStatistic =
+        this.statisticService.calculateAverageScore(monthStatisticValues);
+      await this.statisticService.saveMonthStatistic(
+        currentDayMonthNumber,
+        monthStatistic,
+        user,
+      );
+    }
+  }
+
   private getDateRange(startDate: Date, endDate: Date): Date[] {
     const dates: Date[] = [];
     const currentDate = new Date(startDate);
+    currentDate.setHours(23, 59, 59, 999);
 
-    while (currentDate <= endDate) {
+    while (currentDate < endDate) {
       dates.push(new Date(currentDate));
       currentDate.setDate(currentDate.getDate() + 1);
     }
@@ -47,31 +113,37 @@ export class MaintenanceService implements OnModuleInit {
   }
 
   private getWeekRange(date: Date): { startDate: Date; endDate: Date } {
-    const startOfWeek = new Date(date);
-    const endOfWeek = new Date(date);
+    const inputDate = new Date(
+      Date.UTC(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate()),
+    );
 
-    const dayOfWeek = startOfWeek.getDay();
-    const diff = dayOfWeek === 0 ? 6 : dayOfWeek - 1;
+    const dayOfWeek = inputDate.getUTCDay();
+    const diff = (dayOfWeek + 6) % 7;
 
-    startOfWeek.setDate(startOfWeek.getDate() - diff);
-    startOfWeek.setHours(0, 0, 0, 0);
+    const startOfWeek = new Date(inputDate);
+    startOfWeek.setUTCDate(startOfWeek.getUTCDate() - diff);
+    startOfWeek.setUTCHours(0, 0, 0, 0);
 
-    endOfWeek.setDate(endOfWeek.getDate() + (6 - diff));
-    endOfWeek.setHours(23, 59, 59, 999);
+    const endOfWeek = new Date(startOfWeek);
+    endOfWeek.setUTCDate(endOfWeek.getUTCDate() + 6);
+    endOfWeek.setUTCHours(23, 59, 59, 999);
 
     return { startDate: startOfWeek, endDate: endOfWeek };
   }
 
   private getMonthRange(date: Date): { startDate: Date; endDate: Date } {
-    const startOfMonth = new Date(date);
-    const endOfMonth = new Date(date);
+    const inputDate = new Date(
+      Date.UTC(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate()),
+    );
 
-    startOfMonth.setDate(1);
-    startOfMonth.setHours(0, 0, 0, 0);
+    const startOfMonth = new Date(inputDate);
+    startOfMonth.setUTCDate(1);
+    startOfMonth.setUTCHours(0, 0, 0, 0);
 
-    endOfMonth.setMonth(endOfMonth.getMonth() + 1);
-    endOfMonth.setDate(0);
-    endOfMonth.setHours(23, 59, 59, 999);
+    const endOfMonth = new Date(inputDate);
+    endOfMonth.setUTCMonth(endOfMonth.getUTCMonth() + 1);
+    endOfMonth.setUTCDate(0);
+    endOfMonth.setUTCHours(23, 59, 59, 999);
 
     return { startDate: startOfMonth, endDate: endOfMonth };
   }
@@ -94,5 +166,14 @@ export class MaintenanceService implements OnModuleInit {
 
   private getMonthNumber(date: Date): number {
     return date.getMonth() + 1;
+  }
+
+  private getYearNumber(date: string | Date): number {
+    const parsedDate = new Date(date);
+    return parsedDate.getFullYear();
+  }
+
+  private getDaysInMonth(year: number, month: number): number {
+    return new Date(year, month, 0).getDate();
   }
 }
